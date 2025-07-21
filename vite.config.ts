2cefedfd44dbeb0982db7d32dd7fd41f621e8a3b
Lamiabrc <sabullelam@gmail.com>
2025-07-22 01:02:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
@@ -10,14 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '/',
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-  // 🛠️ LE TRUC LE PLUS IMPORTANT :
-  // Pour dire à Vercel ou Netlify : redirige toutes les routes vers index.html
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
